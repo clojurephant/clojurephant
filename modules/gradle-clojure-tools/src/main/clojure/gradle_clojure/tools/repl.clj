1e9d2f60cb00ff541a47543536c7dacf95e165a9
Andrew Oberstar <ajoberstar@gmail.com>
2017-10-28 15:52:10 -0500

    Basic socket REPL support
    Adds a new task for running a Clojure REPL. Will stay running until you
    press Ctrl-D. It can choose a random port, if you don't specify one.

---
+++ modules/gradle-clojure-tools/src/main/clojure/gradle_clojure/tools/repl.clj
@@ -0,0 +1,16 @@
+(ns gradle-clojure.tools.repl
+  (:require [clojure.core.server :as server]))
+
+(def stopper (atom nil))
+
+(defn start! [port]
+  (reset! stopper (promise))
+  (server/start-server {:name "repl"
+                        :port port
+                        :accept 'clojure.core.server/repl})
+  (deref (deref stopper))
+  (server/stop-server "repl"))
+
+(defn stop! []
+  ; message doesn't matter, we're just letting them know it's done
+  (deliver @stopper "stop"))

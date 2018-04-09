8aa0f4e45ca4d115b52660c643cdcf7d08c6ca8e
Andrew Oberstar <ajoberstar@gmail.com>
2018-04-08 20:40:27 -0500

    Fix tests for line number in reflection warning

--- modules/gradle-clojure-plugin/src/compatTest/clojure/gradle_clojure/compat_test/basic_clojure.clj
+++ modules/gradle-clojure-plugin/src/compatTest/clojure/gradle_clojure/compat_test/basic_clojure.clj
@@ -40,18 +40,18 @@
     (testing "without reflection warnings enabled, nothing is output"
       (let [result (gradle/build "clean" "compileClojure")]
         (is (= TaskOutcome/SUCCESS (some-> result (.task ":compileClojure") .getOutcome)))
-        (is (not (str/includes? (.getOutput result) "Reflection warning, basic_project/core.clj:27")))))
+        (is (not (str/includes? (.getOutput result) "Reflection warning, basic_project/core.clj:11")))))
     (testing "with reflection warnings enabled"
       (file/write-str (gradle/file "build.gradle") "compileClojure { options.reflectionWarnings.enabled = true }\n" :append true)
       (testing "and asErrors false, warnings are only output"
         (let [result (gradle/build "clean" "compileClojure")]
           (is (= TaskOutcome/SUCCESS (some-> result (.task ":compileClojure") .getOutcome)))
-          (is (str/includes? (.getOutput result) "Reflection warning, basic_project/core.clj:27"))))
+          (is (str/includes? (.getOutput result) "Reflection warning, basic_project/core.clj:11"))))
       (testing "and asErrors true, warnings cause build to fail"
         (file/write-str (gradle/file "build.gradle") "compileClojure { options.reflectionWarnings.asErrors = true }\n" :append true)
         (let [result (gradle/build-and-fail "clean" "compileClojure")]
           (is (= TaskOutcome/FAILED (some-> result (.task ":compileClojure") .getOutcome)))
-          (is (str/includes? (.getOutput result) "Reflection warning, basic_project/core.clj:27")))))))
+          (is (str/includes? (.getOutput result) "Reflection warning, basic_project/core.clj:11")))))))
 
 (deftest clojure-test
   (testing "clojure.test failures cause the build to fail"

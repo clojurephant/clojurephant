71e11d4a358c78ea45b7d216059628bcb23b0f73
Andrew Oberstar <andrew.oberstar@outlook.com>
2021-09-06 14:04:21 -0500

    Latest plugins

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,11 +2,11 @@ pluginManagement {
   plugins {
     id("org.ajoberstar.grgit") version("4.1.0")
     id("org.ajoberstar.reckon") version("0.13.0")
-    id("com.diffplug.spotless") version("5.9.0")
+    id("com.diffplug.spotless") version("5.15.0")
 
-    id("dev.clojurephant.clojure") version("0.6.0-beta.1")
+    id("dev.clojurephant.clojure") version("0.6.0")
     id("org.ajoberstar.stutter") version("0.6.0")
-    id("com.gradle.plugin-publish") version("0.12.0")
+    id("com.gradle.plugin-publish") version("0.15.0")
   }
 }
 

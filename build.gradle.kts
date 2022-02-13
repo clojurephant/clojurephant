a4d7706219f927f540636ca7419d2687607e6c6f
Andrew Oberstar <andrew.oberstar@outlook.com>
2022-02-13 09:57:57 -0600

    chore(build): Enable build scans

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,3 +8,9 @@ reckon {
   setScopeCalc(calcScopeFromProp().or(calcScopeFromCommitMessages()))
   setStageCalc(calcStageFromProp())
 }
+
+// accept build scan terms
+extensions.findByName("buildScan")?.withGroovyBuilder {
+  setProperty("termsOfServiceUrl", "https://gradle.com/terms-of-service")
+  setProperty("termsOfServiceAgree", "yes")
+}

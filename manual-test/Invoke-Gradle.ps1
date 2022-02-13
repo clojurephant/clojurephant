53c055557153688c57550d86b152dc69e1c77d4e
Andrew Oberstar <andrew.oberstar@outlook.com>
2022-02-13 13:53:24 -0600

    chore(build): Update manual-test project to 7.4
    Gets the manual test project updated to the latest Gradle and uses
    composite builds instead of the Invoke-Gradle.ps1 workaround.

--- manual-test/Invoke-Gradle.ps1
+++
@@ -1,7 +0,0 @@
-Write-Host 'Running clojurephant...'
-Push-Location -Path '..'
-& ./gradlew publishToMavenLocal
-Pop-Location
-
-Write-Host 'Running manual-test...'
-& ./gradlew --refresh-dependencies $args

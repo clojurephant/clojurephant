cc68a98d118ffe7c7748163106ee2f0c90515fee
Andrew Oberstar <andrew@ajoberstar.org>
2023-12-31 11:10:30 -0600

    Update sample to use tooling JAR

--- manual-test/src/dev/clojure/dev.clj
+++ manual-test/src/dev/clojure/dev.clj
@@ -1,16 +1,6 @@
 (ns dev
   (:require [com.stuartsierra.component.repl :refer [reset set-init start stop system]]
             [sample.core :as core]
-            [cider.piggieback :as piggie]
-            [figwheel.repl :as frepl]))
+            [dev.clojurephant.tooling.figwheel-main :as fig]))
 
 (set-init core/make-system)
-
-(defonce env
-  (frepl/repl-env
-   :port 5050
-   :open-url "http://localhost:8080/index.html"
-   :output-dir "build/tmp/clojureRepl/public/js/out"))
-
-(defn cljs-repl []
-  (piggie/cljs-repl env))

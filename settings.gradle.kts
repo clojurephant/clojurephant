bd220869cbbc4e8666f33b56d54d1ad653cf5dfb
Andrew Oberstar <andrew@ajoberstar.org>
2024-10-12 21:30:07 -0500

    Update to latest Gradle 8.10.2
    And update plugins used by the build.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,11 +1,11 @@
 pluginManagement {
   plugins {
-    id("dev.clojurephant.clojure") version("0.8.0-beta.4")
+    id("dev.clojurephant.clojure") version("0.8.0-beta.7")
 
-    id("org.ajoberstar.reckon.settings") version("0.18.2")
-    id("com.diffplug.spotless") version("6.23.3")
-    id("org.ajoberstar.stutter") version("0.7.3")
-    id("com.gradle.plugin-publish") version("1.2.1")
+    id("org.ajoberstar.reckon.settings") version("0.18.3")
+    id("com.diffplug.spotless") version("6.25.0")
+    id("org.ajoberstar.stutter") version("1.0.0")
+    id("com.gradle.plugin-publish") version("1.3.0")
   }
 }
 
